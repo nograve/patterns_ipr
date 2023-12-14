89af72ed5f0a25c262e9c71aff9446f8ecba4603
ihor.zakharov <ihor.zakharov@cleveroad.com>
2023-12-14 08:01:47 +0200

    Created page for patterns

---
+++ lib/pages/pattern/pattern_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PatternPage extends StatelessWidget {
+  const PatternPage({super.key});
+
+  static const routeName = '/pattern';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Pattern'),
+    );
+  }
+}
